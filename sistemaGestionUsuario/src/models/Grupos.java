c53575da9e5e21c63d5effdee61105246b1a042d
JRodriguez <jefrodriguezmorales@gmail.com>
2024-10-10 11:55:45 +0200

    Carpeta Entidades

--- sistemaGestionUsuario/src/models/Grupos.java
+++ sistemaGestionUsuario/src/models/Grupos.java
@@ -1,4 +1,47 @@
 package models;
 
 public class Grupos {
+
+    private int id;
+
+    private String nombre;
+
+    private String descripcion;
+
+
+    private Grupos(){
+        id=1;
+        nombre="grupo 1";
+        descripcion="descripcion";
+    }
+
+    public Grupos(int id,String nombre, String descripcion){
+        setId(id);
+        setNombre(nombre);
+        setDescripcion(descripcion);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- sistemaGestionUsuario/src/models/Roles.java
+++ sistemaGestionUsuario/src/models/Roles.java
@@ -1,4 +1,46 @@
 package models;
 
 public class Roles {
+    private int id;
+
+    private String nombre;
+
+    private String permisos;
+
+    private Roles(){
+        id=1;
+        nombre="nombre";
+        permisos="permisos";
+    }
+
+    public Roles(int id,String nombre, String permisos){
+        super();
+        setId(id);
+        setNombre(nombre);
+        setPermisos(permisos);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(String permisos) {
+        this.permisos = permisos;
+    }
 }
